f4c82698ba747d28f488842a5dc07590a0105432
Pedro Costa <a21904825@alunos.ulht.pt>
2020-11-15 15:12:58 +0000

    função startgame parcialmente feita

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -1,4 +1,55 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class TWDGameManager {
+    int[] linhaColuna = new int[2];
+    int idEquipaStart;
+    int criaturasJogo;
+    ArrayList<Humano> listHumanos = new ArrayList<>();
+    ArrayList<Zombie> listZombie = new ArrayList<>();
+
+    public boolean startGame(File ficheiroInicial) {
+        int count = 0;
+        String linha;
+        String[] dados;
+        try {
+            Scanner leitorFicheiro = new Scanner(ficheiroInicial);
+            while(leitorFicheiro.hasNextLine()) {
+                switch (count) {
+                    case 0:
+                        linha = leitorFicheiro.nextLine();
+                        dados = linha.split(" ");
+                        linhaColuna[0] = Integer.parseInt(dados[0]);
+                        linhaColuna[1] = Integer.parseInt(dados[1]);
+                        count++;
+                        break;
+                    case 1:
+                        linha = leitorFicheiro.nextLine();
+                        idEquipaStart = Integer.parseInt(linha);
+                        count++;
+                        break;
+                    case 2:
+                        linha = leitorFicheiro.nextLine();
+                        criaturasJogo = Integer.parseInt(linha);
+                        count++;
+                        break;
+                    case 3:
+                        for (int i = 0;i<3;i++) {
+
+                        }
+                        count++;
+                        break;
+                }
+            }
+            leitorFicheiro.close();
+        }
+        catch(FileNotFoundException exception) {
+            System.out.println("Erro: o ficheiro nao foi encontrado.");
+        }
+        return false;
+    }
 }
