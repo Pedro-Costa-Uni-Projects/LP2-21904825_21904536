7b870aabfac3e74525ab7f7dbb09237745c1144b
a21904536 <a21904536@alunos.ulht.pt>
2020-12-22 16:58:28 +0000

    Pergunta Ash: A noite dos Mortos-Vivos

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -455,7 +455,7 @@ public class TWDGameManager {
     public String[] popCultureExtravaganza() {
         String[] respostas = new String[14];
         respostas[0] = "Resident Evil";
-        respostas[1] = "Army of Darkness";
+        respostas[1] = "A Noite dos Mortos-Vivos";
         respostas[2] = "The Night Eats the World";
         respostas[3] = "";
         respostas[4] = "";
