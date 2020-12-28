1eb7417ae297cc93d1442871daff2ad9c0d5bd75
a21904536 <a21904536@alunos.ulht.pt>
2020-12-28 00:44:07 +0000

    Pop

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -653,7 +653,7 @@ public class TWDGameManager {
         respostas[1] = "Evil Dead";
         respostas[2] = "I Am Legend";
         respostas[3] = "I Am Legend";
-        respostas[4] = "Heroes of the Storm";
+        respostas[4] = "Dungeons & Dragons";
         respostas[5] = "Resident Evil";
         respostas[6] = "Mandalorianos";
         respostas[7] = "1972";
